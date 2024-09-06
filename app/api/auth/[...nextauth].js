d73c8ef41d1889ee49ebce6a449eca639ff4e5b0
Vinay Kumar <vinayvidhehi@gmail.com>
2024-09-06 19:16:56 +0530

    slight hopes

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -7,6 +7,11 @@ import GoogleProvider from "next-auth/providers/google";
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID ,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+            params: {
+              prompt: 'select_account',
+            },
+          },
       }),
     ],
     callbacks: {
