9d168bd528feb9cab020ef804b6d395059910d0b
Vinay Kumar <vinayvidhehi@gmail.com>
2024-09-06 22:47:38 +0530

    no hopes at all

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,20 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-  
   export const authOptions = {
     // Configure one or more authentication providers
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID ,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri:process.env.GOOGLE_REDIRECT_URI,
         authorization: {
-          params: {
-            prompt: 'select_account',
+            params: {
+              scope: "openid email profile",
+              prompt: "select_account",
+              ack_oob_shutdown: "2022-10-03",  // Acknowledge OOB shutdown
+            },
           },
-        },
       }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
@@ -28,7 +30,8 @@ import GoogleProvider from "next-auth/providers/google";
         }
         return true; // Allow sign-in for other providers
       },
-    }
+    },
+
   };
 
 
